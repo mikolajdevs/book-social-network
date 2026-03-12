0ec1ef71218999659d87aa1397cd15c929c82868
mikolajdevs <mikolajdevs@gmail.com>
2026-03-12 23:16:35 +0100

    feature: add user, role entities [backend]

---
+++ book-network/src/main/java/com/example/book/user/TokenRepository.java
@@ -0,0 +1,12 @@
+package com.example.book.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Integer> {
+
+    Optional<Token> findByToken(String token);
+}
