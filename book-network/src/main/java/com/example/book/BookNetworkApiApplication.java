3134b46bc92f48a6529e64850f3ed43a9394f3ea
mikolajdevs <mikolajdevs@gmail.com>
2026-03-17 00:09:42 +0100

    util: add role USER on start up if not exists [backend]

--- book-network/src/main/java/com/example/book/BookNetworkApiApplication.java
+++ book-network/src/main/java/com/example/book/BookNetworkApiApplication.java
@@ -1,7 +1,11 @@
 package com.example.book;
 
+import com.example.book.role.Role;
+import com.example.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -10,8 +14,18 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class BookNetworkApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookNetworkApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookNetworkApiApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.findByName("USER").isEmpty()) {
+                roleRepository.save(
+                        Role.builder().name("USER").build()
+                );
+            }
+        };
+    }
 }
