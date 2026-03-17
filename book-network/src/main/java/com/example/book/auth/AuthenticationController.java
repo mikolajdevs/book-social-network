50c894bb289bc183181211126643b3ecfc087aa5
mikolajdevs <mikolajdevs@gmail.com>
2026-03-17 20:25:13 +0100

    feature: add registration api [backend]

--- book-network/src/main/java/com/example/book/auth/AuthenticationController.java
+++ book-network/src/main/java/com/example/book/auth/AuthenticationController.java
@@ -18,7 +18,7 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public ResponseEntity<?> register(@RequestBody @Valid final RegistrationRequest request) throws MessagingException {
+    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
         service.register(request);
         return ResponseEntity.accepted().build();
     }
