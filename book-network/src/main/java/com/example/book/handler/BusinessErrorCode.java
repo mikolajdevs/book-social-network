ad77402bc194bad5cb54284776277ba7a36ab4cd
mikolajdevs <mikolajdevs@gmail.com>
2026-03-17 22:49:44 +0100

    feature: add global exception handler [backend]

---
+++ book-network/src/main/java/com/example/book/handler/BusinessErrorCode.java
@@ -0,0 +1,24 @@
+package com.example.book.handler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+
+@Getter
+@AllArgsConstructor
+public enum BusinessErrorCode {
+    NO_CODE(0, NOT_IMPLEMENTED, "No code"),
+    INCORRECT_CURRENT_PASSWORD(300, BAD_REQUEST, "Current password is incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301, BAD_REQUEST, "New password does not match"),
+    ACCOUNT_LOCKED(302, FORBIDDEN, "User account is locked"),
+    ACCOUNT_DISABLED(303, FORBIDDEN, "User account is disabled"),
+    BAD_CREDENTIALS(304, FORBIDDEN, "Login and / or password is incorrect"),
+    ;
+
+    private final int code;
+    private final HttpStatus httpStatus;
+    private final String description;
+}
